9634d0faf79ecf774dd2be67ce6753a39cf1292e
Yousuf Machiyara <121059585+myousufmachiyara@users.noreply.github.com>
2026-01-24 09:37:00 +0500

    fixes

--- resources/views/vouchers/index.blade.php
+++ resources/views/vouchers/index.blade.php
@@ -226,6 +226,7 @@
   }
 
   $(document).ready(function() {
+    $('.select2-js').select2({ width: '100%' });
     $('#voucher-datatable').DataTable({
       pageLength: 50,
       order: [[0, 'desc']],
